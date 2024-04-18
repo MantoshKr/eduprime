fa84e985e3cb769c8c0978ffb443bc9685fcf9c6
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2024-04-19 04:56:35 +0530

    client initial commit
    configured next js and tailwind css

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Index = () => {
+  return (
+    <div className='text-3xl text-blue-600 font-extrabold'
+    >This is LMS project</div>
+  );
+}
+
+export default Index
