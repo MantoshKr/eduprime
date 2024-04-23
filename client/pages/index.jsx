e548b46d21a3c6694e8d24f06965c7184e7d8089
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2024-04-24 02:07:46 +0530

    added carousel to the homepage

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-
+import React from "react";
+import Carousel from "../components/Carousel";
 
 const Index = () => {
   return (
-    <div className='text-3xl text-blue-600 font-bold'
-    >Home Page</div>
+    <div>
+      <Carousel />
+      <div className="mt-5 bg-slate-100 p-10 rounded-2xl">
+        <p className="text-4xl font-bold">
+          All the skills you need in one place
+        </p>
+        <p className="my-3 text-xl">
+          From critical skills to technical topics, EduPrime supports your
+          professional development.
+        </p>
+      </div>
+    </div>
   );
-}
+};
 
-export default Index
+export default Index;
