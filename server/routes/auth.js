8440a7968b9458ddc4f13eb2405127f2243f6374
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2024-04-28 16:54:42 +0530

    controller setup

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,12 @@ import express from "express"
 
 const router = express.Router();
 
-router.get("/register", (req, res)=>{
-    res.send("register user")
-});
+// router.get("/register", (req, res)=>{
+//     res.send("register user")
+// });
+
+import { register } from "../controllers/auth";
+
+router.get("/register", register)
 
 module.exports = router;
