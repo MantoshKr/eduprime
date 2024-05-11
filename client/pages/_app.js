2a25059a4920d455eea14c25d91d1b7e4791a736
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2024-05-11 19:23:44 +0530

    Commit Message: Added global state management with Context and Reducer hooks
    Description:
    
    Implemented global state management using React Context and Reducer hooks.
    Enabled user information preservation across page refresh and redirect .

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,10 +2,11 @@ import "../index.css"
 import Navbar from "../components/Navbar";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from "../context";
 
 function MyApp({Component, pageProps}){
     return (
-    <>
+    <Provider >
     <Navbar />
    
      <div className="mx-auto max-w-screen-2xl px-4 mt-1">
@@ -14,7 +15,7 @@ function MyApp({Component, pageProps}){
     </div>
     <ToastContainer position="top-right" />
     
-    </>
+    </Provider>
     )
 }
 
